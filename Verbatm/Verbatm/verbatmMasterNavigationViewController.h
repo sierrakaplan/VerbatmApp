56dfc6e05395bf7f248506e81fdfe09299ed85d3
iusiri <iusiri@stanford.edu>
2015-06-05 23:55:45 -0700

    working on the restructure

---
+++ Verbatm/Verbatm/verbatmMasterNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  verbatmMasterNavigationViewController.h
+//  Verbatm
+//
+//  Created by Iain Usiri on 5/20/15.
+//  Copyright (c) 2015 Verbatm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface verbatmMasterNavigationViewController : UIViewController
+
+@end
---
+++ Verbatm/Verbatm/verbatmMasterNavigationViewController.m
@@ -0,0 +1,37 @@
+//
+//  verbatmMasterNavigationViewController.m
+//  Verbatm
+//
+//  Created by Iain Usiri on 5/20/15.
+//  Copyright (c) 2015 Verbatm. All rights reserved.
+//
+
+#import "verbatmMasterNavigationViewController.h"
+
+@interface verbatmMasterNavigationViewController ()
+
+@end
+
+@implementation verbatmMasterNavigationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
