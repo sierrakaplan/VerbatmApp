378b354c5903658791955cbc6ab6a36e7f195876
iusiri <iusiri@stanford.edu>
2015-08-31 14:42:39 -0700

    merging backend

--- Verbatm/Verbatm/articleLoadAndDisplayManager.h
+++ Verbatm/Verbatm/articleLoadAndDisplayManager.h
@@ -12,19 +12,19 @@
 /*This class manages the entire article stream as well as turning them into presentable views*/
 
 @protocol articleLoadAndDisplayProtocol <NSObject>
--(void)rightArticleDidLoad:(singleArticlePresenter *) articleView;
--(void)leftArticleDidLoad:(singleArticlePresenter *) articleView;
+    -(void)rightArticleDidLoad:(singleArticlePresenter *) articleView;
+    -(void)leftArticleDidLoad:(singleArticlePresenter *) articleView;
 @end
 
 @interface articleLoadAndDisplayManager : NSObject
-@property (strong, nonatomic) id<articleLoadAndDisplayProtocol> articleLDDelegate;
-@property (strong, nonatomic, readonly) NSArray * articleList;
+    @property (strong, nonatomic) id<articleLoadAndDisplayProtocol> articleLDDelegate;
+    @property (strong, nonatomic, readonly) NSArray * articleList;
 
-/*These two functions instruct our model to load and provide the next article to the left or right of the one presented
- They do not return anything and are reacted to through the protocol described above
- */
--(void)getRightArticle;
--(void)getLeftArticle;
--(BOOL)fetchArticleWithIndex:(NSInteger) index withFrame:(CGRect)frame onCompletion:(void(^)(singleArticlePresenter *))completionBlock;
--(void)reloadArticleListWithCompletionBlock:(void (^)(void))onCompletion;
+    /*These two functions instruct our model to load and provide the next article to the left or right of the one presented
+     They do not return anything and are reacted to through the protocol described above
+     */
+    -(void)getRightArticle;
+    -(void)getLeftArticle;
+    -(BOOL)fetchArticleWithIndex:(NSInteger) index withFrame:(CGRect)frame onCompletion:(void(^)(singleArticlePresenter *))completionBlock;
+    -(void)reloadArticleListWithCompletionBlock:(void (^)(void))onCompletion;
 @end
