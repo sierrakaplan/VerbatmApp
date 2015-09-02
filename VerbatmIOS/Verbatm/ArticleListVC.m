f9bac541955fe5c7f7c29e05825795cb55814398
iusiri <iusiri@stanford.edu>
2015-09-02 13:39:34 -0700

    article list changed

--- VerbatmIOS/Verbatm/ArticleListVC.m
+++ VerbatmIOS/Verbatm/ArticleListVC.m
@@ -28,145 +28,125 @@
 #define VIEW_ARTICLE_SEGUE @"viewArticleSegue"
 
 @interface ArticleListVC ()<UITableViewDataSource, UITableViewDelegate>
-    @property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
-    @property (strong, nonatomic) UIButton *composeStoryButton;
-    @property (nonatomic) BOOL cellSet;
-    //we maintain the cell height so that we can set the height of the placeholderCell
-    @property (nonatomic) CGFloat cellHeight;
-    @property (weak, nonatomic) IBOutlet UILabel *listTitle;
-    @property (strong, nonatomic) FeedTableView *storyListView;
-    //this cell is inserted in the top of the listview
-	@property (strong,nonatomic) FeedTableViewCell* placeholderCell;
-    @property BOOL pullDownInProgress;
-    //tells you wether or not we have started a timer to animate
-    @property (atomic) BOOL refreshInProgress;
-    @property  (nonatomic) NSInteger selectedArticleIndex;
-    @property (strong, nonatomic) articleLoadAndDisplayManager * articleLoadManger;
-    #define SHC_ROW_HEIGHT 20.f
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) UIButton *composeStoryButton;
+@property (nonatomic) BOOL cellSet;
+//we maintain the cell height so that we can set the height of the placeholderCell
+@property (nonatomic) CGFloat cellHeight;
+@property (weak, nonatomic) IBOutlet UILabel *listTitle;
+@property (strong, nonatomic) FeedTableView *storyListView;
+//this cell is inserted in the top of the listview
+@property (strong,nonatomic) FeedTableViewCell* placeholderCell;
+@property BOOL pullDownInProgress;
+//tells you wether or not we have started a timer to animate
+@property (atomic) BOOL refreshInProgress;
+@property  (nonatomic) NSInteger selectedArticleIndex;
+@property (strong, nonatomic) articleLoadAndDisplayManager * articleLoadManger;
+#define SHC_ROW_HEIGHT 20.f
 @end
 
 @implementation ArticleListVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	[self addBlurView];
-	[self initStoryListView];
-	//[self addComposeStoryButton];
-	[self registerForNavNotifications];
+    [self initStoryListView];
+    [self registerForNavNotifications];
 }
 
 -(void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-	[self refreshFeed];
-}
-
--(void) viewDidDisappear:(BOOL)animated {
-	[super viewDidDisappear:animated];
+    //[self refreshFeed];
+    [self.storyListView performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
 }
 
 -(void) initStoryListView {
-	self.storyListView = [[FeedTableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-	[self.storyListView setBackgroundColor:[UIColor clearColor]];
+    self.storyListView = [[FeedTableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    [self.storyListView setBackgroundColor:[UIColor clearColor]];
     self.storyListView.separatorStyle = UITableViewCellSeparatorStyleNone;
-	self.storyListView.dataSource = self;
-	self.storyListView.delegate = self;
-	self.placeholderCell = [[FeedTableViewCell alloc] init];
-	[self.view addSubview:self.storyListView];
+    self.storyListView.dataSource = self;
+    self.storyListView.delegate = self;
+    [self.view addSubview:self.storyListView];
 }
 
--(void) addBlurView {
-//	[self.view insertSubview: self.verbatmCameraView atIndex:0];
-	UIImageView* backgroundImageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
-	UIImage* backgroundImage = [UIImage imageNamed:@"placeholder_background_image"];
-	backgroundImageView.image = backgroundImage;
-    backgroundImageView.clipsToBounds = YES;
-	[backgroundImageView setContentMode:UIViewContentModeScaleAspectFill];
-	[self.view insertSubview:backgroundImageView atIndex:0];
 
-	UIView* blurView = [[UIView alloc] initWithFrame:self.view.bounds];
-	[UIEffects createBlurViewOnView:blurView withStyle:UIBlurEffectStyleDark];
-	[self.view addSubview:blurView];
-}
-
--(void) addComposeStoryButton {
-	UIColor* buttonColor = [UIColor colorWithWhite:1 alpha:0.6];
-	self.composeStoryButton = [UIButton buttonWithType:UIButtonTypeCustom];
-	CGRect composeStoryButtonFrame = CGRectMake(self.view.bounds.size.width - COMPOSE_STORY_BUTTON_SIZE - COMPOSE_STORY_BUTTON_OFFSET,
-												self.view.bounds.size.height - COMPOSE_STORY_BUTTON_SIZE - COMPOSE_STORY_BUTTON_OFFSET,
-												COMPOSE_STORY_BUTTON_SIZE, COMPOSE_STORY_BUTTON_SIZE);
-	[self.composeStoryButton setFrame: composeStoryButtonFrame];
-	self.composeStoryButton.backgroundColor = buttonColor;
-	self.composeStoryButton.layer.cornerRadius = COMPOSE_STORY_BUTTON_SIZE/2.f;
-
-	//set attributed title
-	UIColor *labelColor = [UIColor COMPOSE_STORY_BUTTON_LABEL_COLOR];
-	UIFont* labelFont = [UIFont fontWithName:BUTTON_FONT size:COMPOSE_STORY_BUTTON_LABEL_FONT_SIZE];
-	[self.composeStoryButton setTitle:COMPOSE_STORY_BUTTON_LABEL forState:UIControlStateNormal];
-	[self.composeStoryButton setTitleColor:labelColor forState:UIControlStateNormal];
-
-	self.composeStoryButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
-	self.composeStoryButton.titleLabel.numberOfLines = 2;
-	self.composeStoryButton.titleLabel.textColor = labelColor;
-	self.composeStoryButton.titleLabel.font = labelFont;
-
-	[self.composeStoryButton addTarget:self action:@selector(composeStory:) forControlEvents:UIControlEventTouchUpInside];
-
-	float originDiff = (COMPOSE_STORY_OUTER_CIRCLE_SIZE - COMPOSE_STORY_BUTTON_SIZE)/2.f;
-	CGRect outerCircleViewFrame = CGRectMake(composeStoryButtonFrame.origin.x - originDiff, composeStoryButtonFrame.origin.y - originDiff, COMPOSE_STORY_OUTER_CIRCLE_SIZE, COMPOSE_STORY_OUTER_CIRCLE_SIZE);
-	UIView* outerCircleView = [[UIView alloc] initWithFrame:outerCircleViewFrame];
-	outerCircleView.backgroundColor = [UIColor clearColor];
-	outerCircleView.layer.cornerRadius = COMPOSE_STORY_OUTER_CIRCLE_SIZE/2.f;
-	outerCircleView.layer.borderColor = [UIColor whiteColor].CGColor;
-	outerCircleView.layer.borderWidth = COMPOSE_STORY_OUTER_CIRCLE_BORDER_WIDTH;
-
-	[self.view addSubview: outerCircleView];
-	[self.view addSubview:self.composeStoryButton];
-}
+//-(void) addComposeStoryButton {
+//	UIColor* buttonColor = [UIColor colorWithWhite:1 alpha:0.6];
+//	self.composeStoryButton = [UIButton buttonWithType:UIButtonTypeCustom];
+//	CGRect composeStoryButtonFrame = CGRectMake(self.view.bounds.size.width - COMPOSE_STORY_BUTTON_SIZE - COMPOSE_STORY_BUTTON_OFFSET,
+//												self.view.bounds.size.height - COMPOSE_STORY_BUTTON_SIZE - COMPOSE_STORY_BUTTON_OFFSET,
+//												COMPOSE_STORY_BUTTON_SIZE, COMPOSE_STORY_BUTTON_SIZE);
+//	[self.composeStoryButton setFrame: composeStoryButtonFrame];
+//	self.composeStoryButton.backgroundColor = buttonColor;
+//	self.composeStoryButton.layer.cornerRadius = COMPOSE_STORY_BUTTON_SIZE/2.f;
+//
+//	//set attributed title
+//	UIColor *labelColor = [UIColor COMPOSE_STORY_BUTTON_LABEL_COLOR];
+//	UIFont* labelFont = [UIFont fontWithName:BUTTON_FONT size:COMPOSE_STORY_BUTTON_LABEL_FONT_SIZE];
+//	[self.composeStoryButton setTitle:COMPOSE_STORY_BUTTON_LABEL forState:UIControlStateNormal];
+//	[self.composeStoryButton setTitleColor:labelColor forState:UIControlStateNormal];
+//
+//	self.composeStoryButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+//	self.composeStoryButton.titleLabel.numberOfLines = 2;
+//	self.composeStoryButton.titleLabel.textColor = labelColor;
+//	self.composeStoryButton.titleLabel.font = labelFont;
+//
+//	[self.composeStoryButton addTarget:self action:@selector(composeStory:) forControlEvents:UIControlEventTouchUpInside];
+//
+//	float originDiff = (COMPOSE_STORY_OUTER_CIRCLE_SIZE - COMPOSE_STORY_BUTTON_SIZE)/2.f;
+//	CGRect outerCircleViewFrame = CGRectMake(composeStoryButtonFrame.origin.x - originDiff, composeStoryButtonFrame.origin.y - originDiff, COMPOSE_STORY_OUTER_CIRCLE_SIZE, COMPOSE_STORY_OUTER_CIRCLE_SIZE);
+//	UIView* outerCircleView = [[UIView alloc] initWithFrame:outerCircleViewFrame];
+//	outerCircleView.backgroundColor = [UIColor clearColor];
+//	outerCircleView.layer.cornerRadius = COMPOSE_STORY_OUTER_CIRCLE_SIZE/2.f;
+//	outerCircleView.layer.borderColor = [UIColor whiteColor].CGColor;
+//	outerCircleView.layer.borderWidth = COMPOSE_STORY_OUTER_CIRCLE_BORDER_WIDTH;
+//
+//	[self.view addSubview: outerCircleView];
+//	[self.view addSubview:self.composeStoryButton];
+//}
 
 
 -(void)registerForNavNotifications {
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshFeed) name:NOTIFICATION_REFRESH_FEED object: nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setLoadManger:) name:NOTIFICATION_PROPOGATE_ARTICLELOAGMANAGER object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(refreshFeed) name:NOTIFICATION_REFRESH_FEED object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setLoadManger:) name:NOTIFICATION_PROPOGATE_ARTICLELOAGMANAGER object: nil];
 }
 
 
 -(void)setLoadManger:(NSNotification *)notification{
-//    NSDictionary * dict = [notification userInfo];
-//    id am = [dict get];
-//    self.articleLoadManger =
+    NSDictionary * dict = [notification userInfo];
+    self.articleLoadManger = [dict valueForKey:KEY_ARTICLELOAGMANAGER];
 }
 
 
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    self.selectedArticleIndex = indexPath.row;
-    [self viewArticle];
+    //    self.selectedArticleIndex = indexPath.row;
+    //    [self viewArticle];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    if(!self.cellHeight)self.cellHeight =TITLE_LABLE_HEIGHT +4*FEED_TEXT_GAP +USERNAME_LABLE_HEIGHT;
-//    return self.cellHeight;
-    return 100;
-
+    //    if(!self.cellHeight)self.cellHeight =TITLE_LABLE_HEIGHT +4*FEED_TEXT_GAP +USERNAME_LABLE_HEIGHT;
+    //    return self.cellHeight;
+    return 150;
+    
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 
-	FeedTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:FEED_CELL_ID];
-	if (cell == nil) {
-		cell = [[FeedTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:FEED_CELL_ID];
-	}
-    NSInteger index = indexPath.row;
-//    if(!self.pullDownInProgress){
-//        //configure cell
-//        Article * article = self.articleLoadManger.articleList[index];
-//        //[cell setContentWithUsername:[article getAuthorUsername] andTitle:article.title];
-//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-//    }else if(self.refreshInProgress && index ==0){
-//        //this means that the cell is an animation place-holder
-//       // [cell setContentWithUsername:@"" andTitle:@""];
-//        cell.selectionStyle = UITableViewCellSelectionStyleNone;
-//    }
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    FeedTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:FEED_CELL_ID];
+    if (cell == nil) {
+        cell = [[FeedTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:FEED_CELL_ID];
+    }
+    //    NSInteger index = indexPath.row;
+    //    if(!self.pullDownInProgress){
+    //        //configure cell
+    //        Article * article = self.articleLoadManger.articleList[index];
+    //        [cell setContentWithUsername:[article getAuthorUsername] andTitle:article.title];
+    //        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    //    }else if(self.refreshInProgress && index ==0){
+    //this means that the cell is an animation place-holder
+    [cell setContentWithUsername:@"Iain Usiri" andTitle:@"A night in an underwater hotel in Zanzibar"];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    //}
     return cell;
 }
 
@@ -236,7 +216,7 @@
 
 
 -(void)refreshFeed {
-    
+    [self loadContentIntoView];
 }
 
 -(void)loadContentIntoView{
@@ -247,9 +227,10 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    NSUInteger count = self.articleLoadManger.articleList.count;
-    count += (self.pullDownInProgress) ? 1 : 0;
-    return count;
+    //    NSUInteger count = self.articleLoadManger.articleList.count;
+    //    count += (self.pullDownInProgress) ? 1 : 0;
+    //    return count;
+    return 10;
 }
 
 
@@ -257,18 +238,19 @@
     
     float offset_y =scrollView.contentOffset.y ;
     if (self.pullDownInProgress &&  offset_y <=  (-1 * self.cellHeight)) {
-        [self addFinalAnimationTile];
+        // [self addFinalAnimationTile];
     }
     
     
-//    if (self.pullDownInProgress && - scrollView.contentOffset.y > SHC_ROW_HEIGHT) {
-//        [self addFinalAnimationTile];
-//    }
+    //    if (self.pullDownInProgress && - scrollView.contentOffset.y > SHC_ROW_HEIGHT) {
+    //        [self addFinalAnimationTile];
+    //    }
     //they are no longer pulling this down
     self.pullDownInProgress = false;
 }
 
 -(void)addFinalAnimationTile{
+    
     if(!self.refreshInProgress){
         self.refreshInProgress = YES;
         NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
