c4ad4a50829c78688c57b0404d1ce8d2623e7c55
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2015-09-24 20:14:25 -0700

    added frameworks back for parse user auth

---
+++ VerbatmIOS/Verbatm/VerbatmAppDelegate.m
@@ -0,0 +1,83 @@
+//
+//  verbatmAppDelegate.m
+//  Verbatm
+//
+//  Created by Iain Usiri on 8/14/14.
+//  Copyright (c) 2014 Verbatm. All rights reserved.
+//
+
+#import "VerbatmAppDelegate.h"
+
+#import "UserPinchViews.h"
+#import <AVFoundation/AVFoundation.h>
+#import "UserSetupParameters.h"
+
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <Parse/Parse.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+@implementation VerbatmAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+	// [Optional] Power your app with Local Datastore. For more info, go to
+	// https://parse.com/docs/ios_guide#localdatastore/iOS
+	[Parse enableLocalDatastore];
+	// Initialize Parse.
+	[Parse setApplicationId:@"rzSvJWHhiN8KUnhDVXTlapJkJ4eCe3xAlmEscSK3"
+				  clientKey:@"qmXzBTKKMNqm5A3eogopkL2ZY6SeKGcWah0zP9kk"];
+	// [Optional] Track statistics around application opens.
+	[PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+
+	[PFFacebookUtils initializeFacebookWithApplicationLaunchOptions:launchOptions];
+
+    [UserSetupParameters setUpParameters];
+	[[UserPinchViews sharedInstance] loadPinchViewsFromUserDefaults];
+    [[AVAudioSession sharedInstance] setActive: YES error: nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
+    
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application{
+    [[AVAudioSession sharedInstance] setActive: YES error: nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:nil];
+    
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	[FBSDKAppEvents activateApp];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+// Facebook login code
+- (BOOL)application:(UIApplication *)application
+			openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+		 annotation:(id)annotation {
+	return [[FBSDKApplicationDelegate sharedInstance] application:application
+														  openURL:url
+												sourceApplication:sourceApplication
+													   annotation:annotation];
+}
+
+@end
