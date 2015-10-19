42f5b7aa822b1b7746d802b5ecc6712993ae3111
iusiri <iusiri@stanford.edu>
2015-10-19 14:24:49 -0700

    no big chagne

--- VerbatmIOS/Verbatm/EditContentView.m
+++ VerbatmIOS/Verbatm/EditContentView.m
@@ -321,6 +321,49 @@
 				self.textAndImageView.textView.frame.size.height + TOUCH_BUFFER);
 }
 
+
+
+#pragma mark - Filters -
+
+//-(void) setFilteredPhotos {
+//    NSArray* filterNames = [UIImage getPhotoFilters];
+//    self.filteredImages = [[NSMutableArray alloc] initWithCapacity:[filterNames count]+1];
+//    //original photo
+//    [self.filteredImages addObject:self.image];
+//    [self createFilteredImagesFromImage:self.image andFilterNames:filterNames];
+//}
+//
+////return array of uiimage with filter from image
+//-(void)createFilteredImagesFromImage:(UIImage *)image andFilterNames:(NSArray*)filterNames{
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+//        NSData  * imageData = UIImagePNGRepresentation(image);
+//        //Background Thread
+//        for (NSString* filterName in filterNames) {
+//            NSLog(@"Adding filtered photo.");
+//            @autoreleasepool {
+//                CIImage *beginImage =  [CIImage imageWithData: imageData];
+//                CIContext *context = [CIContext contextWithOptions:nil];
+//                CIFilter *filter = [CIFilter filterWithName:filterName keysAndValues: kCIInputImageKey, beginImage, nil];
+//                CIImage *outputImage = [filter outputImage];
+//                CGImageRef CGImageRef = [context createCGImage:outputImage fromRect:[outputImage extent]];
+//                UIImage* imageWithFilter = [UIImage imageWithCGImage:CGImageRef];
+//                CGImageRelease(CGImageRef);
+//                
+//                dispatch_async(dispatch_get_main_queue(), ^{
+//                    [self.filteredImages addObject:imageWithFilter];
+//                });
+//            }
+//        }
+//    });
+//}
+
+
+
+
+
+
+
+
 #pragma mark - lazy instantiation -
 
 -(UIButton *)textCreationButton{
