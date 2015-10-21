ac72fd2641bb64d63ba36ed8a6cf5cd77ea69404
iusiri <iusiri@stanford.edu>
2015-10-20 19:21:25 -0700

    commented out initial analytics

--- VerbatmIOS/Verbatm/VerbatmAppDelegate.m
+++ VerbatmIOS/Verbatm/VerbatmAppDelegate.m
@@ -43,7 +43,7 @@
     [UserSetupParameters setUpParameters];
 	[[UserPovInProgress sharedInstance] loadPOVFromUserDefaults];
     
-    [[Analytics getSharedInstance] newUserSession];
+    //[[Analytics getSharedInstance] newUserSession];
     
     
     return [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
@@ -66,13 +66,13 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    [[Analytics getSharedInstance] endOfUserSession];
+   // [[Analytics getSharedInstance] endOfUserSession];
     
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application{
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    [[Analytics getSharedInstance] newUserSession];
+    //[[Analytics getSharedInstance] newUserSession];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
