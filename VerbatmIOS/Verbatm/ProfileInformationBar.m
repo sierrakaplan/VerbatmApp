0a2b9985a1d0044f9c32ac8df79b4fe78c698e3f
Damas <dam6660@rit.edu>
2016-05-11 12:12:54 -0400

    Initial work on following view

--- VerbatmIOS/Verbatm/ProfileInformationBar.m
+++ VerbatmIOS/Verbatm/ProfileInformationBar.m
@@ -18,6 +18,7 @@
 
 #import "SizesAndPositions.h"
 #import "Styles.h"
+#import "FollowingView.h"
 
 
 @interface ProfileInformationBar ()
@@ -135,7 +136,15 @@
 }
 
 -(void)blockButtonSelected {
-	[self.delegate blockCurrentUserShouldBlock:!self.hasBlockedUser];
+//	[self.delegate blockCurrentUserShouldBlock:!self.hasBlockedUser];
+    FollowingView *V = [[FollowingView alloc] initWithFrame:CGRectMake(0, 0, 375, 667)];
+    
+    UIViewController *top = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    while (top.presentedViewController){
+        top = top.presentedViewController;
+    }
+
+    [top.view addSubview:V];
 }
 
 -(void) createBackButton {
