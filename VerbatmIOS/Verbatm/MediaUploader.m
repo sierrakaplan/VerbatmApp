42d0cf6689132895f313541dc3f5d037c2b234b7
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2016-06-19 12:49:09 -0700

    Deleted commented out code using asihttprequest

--- VerbatmIOS/Verbatm/MediaUploader.m
+++ VerbatmIOS/Verbatm/MediaUploader.m
@@ -26,99 +26,11 @@
 
 @implementation MediaUploader
 
-//@synthesize formData;
-
-//-(instancetype) initWithImage:(NSData*)imageData andUri: (NSString*)uri {
-//	self.formData = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:uri]];
-//	[self.formData setData:imageData
-//			  withFileName:@"defaultImage.png"
-//			andContentType:@"image/png"
-//					forKey:@"defaultImage"];
-//	[self.formData setDelegate:self];
-//	[self.formData setUploadProgressDelegate:self];
-//	[self.formData setTimeOutSeconds: 60];
-//	self.mediaUploadProgress = [NSProgress progressWithTotalUnitCount: IMAGE_PROGRESS_UNITS-1];
-//
-//	return self;
-//}
-////Maybe could do this with Promise NSURLConnection?
-//-(instancetype) initWithVideoData: (NSData*)videoData  andUri: (NSString*)uri {
-//	self.formData = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:uri]];
-//
-//	[self.formData setData:videoData
-//			  withFileName:@"defaultVideo.mp4"
-//			andContentType:@"video/mp4"
-//					forKey:@"defaultVideo"];
-//	[self.formData setDelegate:self];
-//	[self.formData setUploadProgressDelegate:self];
-//	// Needs to be long in order to allow long videos to upload
-//	[self.formData setTimeOutSeconds: 300];
-//	self.mediaUploadProgress = [NSProgress progressWithTotalUnitCount: VIDEO_PROGRESS_UNITS-1];
-//
-//	return self;
-//}
-
 -(instancetype) init {
 	self.operationManager = [AFHTTPSessionManager manager];
 	self.operationManager.responseSerializer = [AFHTTPResponseSerializer serializer];
 	return self;
 }
-
-//-(AnyPromise*) startUpload {
-//	AnyPromise* promise = [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve) {
-//		[self startWithCompletionHandler: ^(NSError* error, NSString* responseURL) {
-//			if (error) {
-//				resolve(error);
-//			} else {
-//				resolve(responseURL);
-//			}
-//		}];
-//	}];
-//
-//	return promise;
-//}
-//
-//-(void) startWithCompletionHandler:(MediaUploadCompletionBlock) completionBlock {
-//	self.completionBlock = completionBlock;
-//	[self.formData startAsynchronous];
-//}
-
-//#pragma mark Delegate methods
-//
-//- (void)request:(ASIHTTPRequest *)request didSendBytes:(long long)bytes {
-//    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-//        if ([request totalBytesSent] > 0) {
-//            float progressAmount = ((float)[request totalBytesSent]/(float)[request postLength]);
-//            NSInteger newProgressUnits = (NSInteger)(progressAmount*(float)self.mediaUploadProgress.totalUnitCount);
-//            if (newProgressUnits != self.mediaUploadProgress.completedUnitCount) {
-//                [[PublishingProgressManager sharedInstance] mediaSavingProgressed:(newProgressUnits - self.mediaUploadProgress.completedUnitCount)];
-//                self.mediaUploadProgress.completedUnitCount = newProgressUnits;
-//                NSLog(@"media upload progress: %ld out of %ld", (long)newProgressUnits, (long)self.mediaUploadProgress.totalUnitCount);
-//            }
-//        }
-//    });
-//}
-//
-//-(void) requestFinished:(ASIHTTPRequest *)request {
-//	//The response string is a blobkeystring and an imagesservice servingurl for image
-//	NSString* responseString = [request responseString];
-//	if (!responseString.length) {
-//		[self requestFailed:request];
-//	} else {
-//		[self.mediaUploadProgress setCompletedUnitCount: self.mediaUploadProgress.totalUnitCount];
-//		self.completionBlock(nil, responseString);
-//	}
-//}
-//
-//-(void) requestFailed:(ASIHTTPRequest *)request {
-//	NSError *error = [request error];
-//	[[Crashlytics sharedInstance] recordError: error];
-//	[[PublishingProgressManager sharedInstance] savingMediaFailedWithError:error];
-//	[self.mediaUploadProgress cancel];
-//	self.completionBlock(error, nil);
-//}
-
-#pragma mark - NSURLSESSION -
 
 -(AnyPromise*) uploadVideoWithUrl:(NSURL*)videoURL andUri:(NSString*)uri {
 	self.mediaUploadProgress = [NSProgress progressWithTotalUnitCount: VIDEO_PROGRESS_UNITS-1];
