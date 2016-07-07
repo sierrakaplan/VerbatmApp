bf4b73bf18f9b6d4a89723c995ff1812ae5e184e
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2016-07-07 15:36:04 -0700

    randomizing discover results

--- VerbatmIOS/Verbatm/UtilityFunctions.m
+++ VerbatmIOS/Verbatm/UtilityFunctions.m
@@ -59,6 +59,18 @@
 	return fusedAsset;
 }
 
++ (NSArray*)shuffleArray:(NSArray*)array {
+
+	NSMutableArray *temp = [[NSMutableArray alloc] initWithArray:array];
+
+	for(NSUInteger i = [array count]; i > 1; i--) {
+		NSUInteger j = (NSUInteger)arc4random_uniform((u_int32_t)i);
+		[temp exchangeObjectAtIndex:i-1 withObjectAtIndex:j];
+	}
+
+	return [NSArray arrayWithArray:temp];
+}
+
 NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 + (NSString *) randomStringWithLength: (NSInteger)length {
