940413452ee41df1c912819aa809b2be1a1d9f71
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2016-08-19 15:16:15 -0700

    breakpoints

---
+++ VerbatmIOS/Verbatm/FollowFriendsVC.h
@@ -0,0 +1,13 @@
+//
+//  FollowFriendsVC.h
+//  Verbatm
+//
+//  Created by Sierra Kaplan-Nelson on 8/19/16.
+//  Copyright © 2016 Verbatm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowFriendsVC : UIViewController
+
+@end
---
+++ VerbatmIOS/Verbatm/FollowFriendsVC.m
@@ -0,0 +1,13 @@
+//
+//  FollowFriendsVC.m
+//  Verbatm
+//
+//  Created by Sierra Kaplan-Nelson on 8/19/16.
+//  Copyright © 2016 Verbatm. All rights reserved.
+//
+
+#import "FollowFriendsVC.h"
+
+@implementation FollowFriendsVC
+
+@end
